499b92bb5d8392b2d7062bedd4997162483f8467
Samuel Couto <samuel.couto92@gmail.com>
2022-12-17 20:19:24 +0100

    Day 1 part 2

--- CalorieCounting.hs
+++ CalorieCounting.hs
@@ -2,20 +2,20 @@ module Main where
 
 import Control.Monad
 import Data.Char
+import Data.List
 import System.Environment
 import System.IO
 import System.IO.Unsafe
 
-aux :: String -> ([Int], Int) -> ([Int], Int)
+aux :: String -> ([Int], [Int]) -> ([Int], [Int])
 aux [] (a, b) =
-    let s = sum a
-    in if s > b
-           then ([], s)
-           else ([], b)
+    ([],(sum a):b)
 aux str (a, b) =
     ((read str) : a, b)
 
 main = do
     contents <- getContents 
-    let result = foldl (\acc x -> aux x acc) ([], 0) (lines contents) 
-    print $ aux "" result
+    let result = foldl (\acc x -> aux x acc) ([], []) (lines contents) 
+        s = aux "" result 
+        r =  sum $ take 3 (reverse (sort (snd s))) 
+    print r
