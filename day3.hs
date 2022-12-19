be18a75ff10cafbac3a0a5736badcc994ec6ed43
Samuel Couto <samuel.couto92@gmail.com>
2022-12-19 23:28:25 +0100

    Day 3 part 2

--- day3.hs
+++ day3.hs
@@ -17,7 +17,21 @@ repeatsLetters a =
             itensMap     = Map.fromList itensZipped
         in nub $ filter (\x->Map.member x (Map.fromList itensZipped)) (snd splitted)  
 
+repeatsLetters2 :: [String] -> String
+repeatsLetters2 l =
+        nub $ foldl intersect (head l) (tail l)
+
+process :: [String] -> [String] -> Int -> Int
+process [] lInitial acc = acc
+process lAtual lInitial acc =
+        let x = head $ repeatsLetters2 lAtual
+            r = if (isUpper x) then (ord x) - 38 else (ord x) - 96
+        in process (take 3 lInitial) (drop 3 lInitial) acc + r 
+
+
 main = do
     contents <- getContents
-    let result = foldl (\acc x-> acc + (sum $ map (\y->if (isUpper y) then (ord y) - 38 else (ord y) - 96) (repeatsLetters x))) 0 (lines contents)
+    --let result = foldl (\acc x-> acc + (sum $ map (\y->if (isUpper y) then (ord y) - 38 else (ord y) - 96) (repeatsLetters x))) 0 (lines contents)
+    let l = lines contents
+        result = process (take 3 l) (drop 3 l) 0
     print result
