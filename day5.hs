13d8e5f7eb075c8cef0bdb0d986eb9795998db18
Samuel Couto <samuel.couto92@gmail.com>
2022-12-26 00:36:51 +0100

    Day 5 part 2

--- day5.hs
+++ day5.hs
@@ -28,13 +28,34 @@ buildStack =
          'R' :-: 'N' :-: 'C' :-: 'L' :-: 'D' :-: 'Z' :-: 'G' :-: EmptyS
     ]
 
-
 buildStackT :: [Stack]
 buildStackT =
     [
          'N' :-: 'Z' :-: EmptyS,
          'D' :-: 'C' :-: 'M' :-: EmptyS,
          'P' :-: EmptyS
+    ]
+
+type Stack2 = [[Char]]
+buildStack2 :: Stack2
+buildStack2 =
+    [
+         ['Q', 'F', 'L', 'S', 'R'],
+         ['T','P', 'G', 'Q', 'Z', 'N'],
+         ['B', 'Q', 'M', 'S'],
+         ['Q', 'B', 'C', 'H', 'J', 'Z', 'G', 'T'],
+         ['S', 'F', 'N', 'B', 'M', 'H', 'P'],
+         ['G', 'V', 'L', 'S', 'N', 'Q', 'C', 'P'],
+         ['F', 'C', 'W'],
+         ['M', 'P', 'V', 'W', 'Z', 'G', 'H', 'Q'],
+         ['R', 'N', 'C', 'L', 'D', 'Z', 'G']
+    ]
+buildStack2T :: Stack2
+buildStack2T =
+    [
+         ['N', 'Z'],
+         ['D', 'C', 'M'],
+         ['P']
     ]
 
 move :: [Stack] -> Int -> Int -> [Stack]
@@ -48,6 +69,17 @@ move ls from to =
                                 then push s2 (fst t)
                                 else v) (fromList  ls)
 
+move2 :: Stack2  -> Int -> Int -> Int -> Stack2
+move2 ls qt from to =
+    let s1 = ls !! from 
+        s2 = ls !! to
+        aux = Prelude.take qt s1 
+    in toList $ mapWithIndex (\i v -> if i == from
+                             then Prelude.drop qt s1 
+                             else if i == to
+                                then aux ++ s2
+                                else v) (fromList  ls)
+
 processMoves :: [Stack] -> Int -> Int -> Int -> [Stack]
 processMoves s m f t =
     foldl (\acc x->move acc f t) s [1..m]
@@ -58,7 +90,10 @@ readPos s p =
 
 main = do
     contents <- getContents
-    let r = foldl (\acc x-> processMoves acc (readPos x 1) (pred $ readPos x 3) (pred $ readPos x 5)) buildStack $ map (words) (lines contents) 
-        a = foldl (\acc x-> (fst (pop x)):acc) [] r
-    print . Prelude.reverse $ a
+    --let r = foldl (\acc x-> processMoves acc (readPos x 1) (pred $ readPos x 3) (pred $ readPos x 5)) buildStack $ map (words) (lines contents) 
+    let r2 = foldl (\acc x-> move2 acc (readPos x 1) (pred $ readPos x 3) (pred $ readPos x 5)) buildStack2 $ map (words) (lines contents)
+        --a = foldl (\acc x-> (fst (pop x)):acc) [] r
+        a = foldr (\x acc -> (head x):acc) [] r2
+    --print . Prelude.reverse $ a
+    print a
 
