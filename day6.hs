46385d0cba5ea8f01be263be0bb57c34770fe81c
Samuel Couto <samuel.couto92@gmail.com>
2022-12-26 18:17:16 +0100

    Day 6 part 2

--- day6.hs
+++ day6.hs
@@ -2,8 +2,8 @@ import Data.List
 
 startOfPacket :: String -> Int -> Int
 startOfPacket s i =
-    if (length (nub $ take 4 s)) == 4
+    if (length (nub $ take 14 s)) == 14
         then
-            i + 4
+            i + 14
         else
             startOfPacket (drop 1 s) i + 1
