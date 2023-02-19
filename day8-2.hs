68c20348bb9ad662bb9918f8939b21d31a185f69
Samuel Couto <samuel.couto92@gmail.com>
2023-02-19 18:48:19 +0100

    refactor: split day8 file in part 1 and part 2

---
+++ day8-2.hs
@@ -0,0 +1,59 @@
+import Data.Sequence
+import Data.Foldable
+import Data.Char
+
+isVisible :: Bool -> [Int] -> Int -> Int -> Bool
+isVisible input l lIndex value =
+    if input == False
+        then False
+    else if (l !! lIndex) >= value
+        then False
+    else True
+
+isVisibleLeft' :: [Int] -> Int -> Int -> Int 
+isVisibleLeft' input value valueIndex =
+    let c = Prelude.length $ takeWhile (< value) (Prelude.reverse (Prelude.take valueIndex input))
+    in if c < (Prelude.length $ Prelude.take valueIndex input)
+           then c + 1
+           else c + 0
+
+isVisibleLeft :: [Int] -> Int -> Int -> Bool 
+isVisibleLeft input value valueIndex =
+    foldl (\acc x -> if acc == False
+                        then False
+                    else if x >= value
+                            then False
+                    else True) True (Prelude.take valueIndex input) 
+
+isVisibleRight :: [Int] -> Int -> Int -> Bool 
+isVisibleRight input value valueIndex =
+    foldl (\acc x -> if acc == False
+                        then False
+                    else if x >= value
+                            then False
+                    else True) True (Prelude.drop (valueIndex + 1) input) 
+
+isVisibleTop :: [[Int]] -> Int -> Int -> Int -> Bool 
+isVisibleTop input value valueIndex index =
+    foldl (\acc x -> isVisible acc x valueIndex value) True (Prelude.take index input) 
+
+isVisibleBottom :: [[Int]] -> Int -> Int -> Int -> Bool 
+isVisibleBottom input value valueIndex index =
+    foldl (\acc x -> isVisible acc x valueIndex value) True (Prelude.drop (index + 1) input) 
+
+process :: [[Int]] -> [Int] -> Int -> Int
+process input l index =
+    sum $ toList $ mapWithIndex(\i v -> if (isVisibleTop input v i index) || (isVisibleBottom input v i index) || (isVisibleLeft l v i) ||(isVisibleRight l v i) then 1 else 0) (fromList l)
+
+run :: [[Int]] -> [Int]
+run l =
+    toList $ mapWithIndex(\i v -> if i == 0
+                then Prelude.length v
+                else if i == (Prelude.length l) - 1
+                    then Prelude.length v
+                    else (process l v i)) (fromList l)
+
+main = do
+    contents <- getContents
+    let l = foldr (\x acc -> (map digitToInt x) : acc) [] (lines contents)
+    print $ sum $ run l
